a8b7d2e03056fe824544ec36ccfee3b121615ea9
Mahmoud Algharbawi <mahmoodgharbawi@hotmail.com>
2023-11-01 00:59:27 -0500

    changed a few things in the code

--- decrypt.py
+++ decrypt.py
@@ -1,10 +1,9 @@
+#import the libraries needed
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import os
 import sys
 
-with open('privateKey.pem', 'rb') as priv:
-    private_key = priv.read()
 
 def decrypt_file(file_path, private_key):
     with open(file_path, 'rb') as encrypted_file:
@@ -26,16 +25,21 @@ def decrypt_directory(directory, private_key):
 
             decrypt_file(file_path, private_key)
 
+# # we will use the command line arguments to get the directory to encrypt
 if len(sys.argv) < 2:
     print("Usage: python decrypt.py <directory>")
     sys.exit(1)
 
 directory = sys.argv[1]
 
+# # if the directory does not exist, we will exit the program
 if not os.path.exists(directory):
     print("Directory does not exist")
     sys.exit(1)
 
 
-
+#reading the private key from the file
+with open('privateKey.pem', 'rb') as priv:
+    private_key = priv.read()
+    
 decrypt_directory(directory, private_key)
